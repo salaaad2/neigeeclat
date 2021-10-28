c7caac80e13b2bfa3270cab2df6ae1bf7e5cc293
salaaad2 <arthurdurant263@gmail.com>
2021-10-28 13:04:16 +0200

    snowcrash DONE

--- level13/level13.c
+++ level13/level13.c
@@ -58,10 +58,42 @@ char * ft_des(char *param_1)
 
 
 int main(int argc, char *argv[]) {
-    char *str;
+  char *str;
 
-    str =  ft_des("boe]!ai0FB@.:|L6l@A?>qJ}I");
-    printf("str : [%s]\n", str);
+  str =  ft_des("boe]!ai0FB@.:|L6l@A?>qJ}I");
+  printf("str : [%s]\n", str);
+  str = ft_des("I`fA>_88eEd:=`85h0D8HE>,D");
+  printf("str : [%s]\n", str);
+  str = ft_des("7`4Ci4=^d=J,?>i;6,7d416,7");
+  printf("str : [%s]\n", str);
+  str = ft_des(")<>B16\\AD<C6,G_<1>^7ci>l4B");
+  printf("str : [%s]\n", str);
+  str = ft_des("B8b:6,3fj7:,;bh>D@>8i:6@D");
+  printf("str : [%s]\n", str);
+  str = ft_des(")?4d@:,C>8C60G>8:h:Gb4?l,A");
+  printf("str : [%s]\n", str);
+  str = ft_des("G8H.6,=4k5J0<cd/D@>>B:>:4");
+  printf("str : [%s]\n", str);
+  str = ft_des("H8B8h_20B4J43><8>\ED<;j@3");
+  printf("str : [%s]\n", str);
+  str = ft_des("78H:J4<4<9i_I4k0J^5>B1j`9");
+  printf("str : [%s]\n", str);
+  str = ft_des("bci`mC{)jxkn<\"uD~6%g7FK`7");
+  printf("str : [%s]\n", str);
+  str = ft_des("Dc6m~;}f8Cj#xFkel;#&ycfbK");
+  printf("str : [%s]\n", str);
+  str = ft_des("74H9D^3ed7k05445J0E4e;Da4");
+  printf("str : [%s]\n", str);
+  str = ft_des("70hCi,E44Df[A4B/J@3f<=:`D");
+  printf("str : [%s]\n", str);
+  str = ft_des("8_Dw\"4#?+3i]q&;p6 gtw88EC");
+  printf("str : [%s]\n", str);
+  str = ft_des("boe]!ai0FB@.:|L6l@A?>qJ}I");
+  printf("str : [%s]\n", str);
+  str = ft_des("g <t61:|4_|!@IF.-62FH&G~DCK/Ekrvvdwz?v|");
+  printf("str : [%s]\n", str);
+  str = ft_des("g <t61:|4_|!@IF.-62FH&G~DCK/Ekrvvdwz?v|");
+  printf("str : [%s]\n", str);
 
-    return 0;
+return 0;
 }
